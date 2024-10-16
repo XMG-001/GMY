cebf164f136ef7941da2ac259101ebc13f117b92
XMG-001 <1691256006@qq.com>
2024-10-16 14:03:03 +0800

    feat: 图片优化

--- src/components/ImgPre.vue
+++ src/components/ImgPre.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <el-button @click="generateFiles(10)" type="primary" style="margin-bottom: 10px;">点击我增加10张图片</el-button>
-        <el-button @click="clearImg" type="danger" style="margin-bottom: 10px;">清除图片</el-button>
+        <el-button @click="generateFiles(10)" type="primary" style="margin-bottom: 10px">点击我增加10张图片</el-button>
+        <el-button @click="clearImg" type="danger" style="margin-bottom: 10px">清除图片</el-button>
         <el-upload drag v-model:file-list="fileList" action="#" list-type="picture-card" :auto-upload="false">
             <el-icon>
                 <Plus />
@@ -11,8 +11,10 @@
                     <el-space direction="vertical" alignment="flex-start">
                         <el-skeleton style="width: 100%" :loading="loading" animated>
                             <template #template>
-                                <el-skeleton-item variant="image"
-                                    style="width: var(--el-upload-list-picture-card-size); height: var(--el-upload-list-picture-card-size)" />
+                                <el-skeleton-item variant="image" style="
+                    width: var(--el-upload-list-picture-card-size);
+                    height: var(--el-upload-list-picture-card-size);
+                  " />
                             </template>
                             <template #default>
                                 <img class="el-upload-list__item-thumbnail" v-lazy="file.url" />
@@ -38,7 +40,6 @@
                     </el-space>
                 </div>
             </template>
-
         </el-upload>
 
         <el-dialog v-model="dialogVisible">
@@ -47,9 +48,9 @@
     </div>
 </template>
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue'
-import { Delete, Download, Plus, ZoomIn } from '@element-plus/icons-vue'
-import type { UploadFile, UploadUserFile } from 'element-plus'
+import { ref, onMounted } from "vue";
+import { Delete, Download, Plus, ZoomIn } from "@element-plus/icons-vue";
+import type { UploadFile, UploadUserFile } from "element-plus";
 import Sortable from "sortablejs";
 
 onMounted(() => {
@@ -58,71 +59,79 @@ onMounted(() => {
 
 // tabs 拖拽排序
 const tabsDrop = () => {
-    Sortable.create(document.querySelector(".el-upload-list--picture-card") as HTMLElement, {
-        draggable: ".el-upload-list__item",
-        animation: 500,
-        onEnd({ newIndex, oldIndex }) {
-            const newList = [...fileList.value];
-            const currRow = newList.splice(oldIndex as number, 1)[0];
-            newList.splice(newIndex as number, 0, currRow);
-            fileList.value = newList;
+    Sortable.create(
+        document.querySelector(".el-upload-list--picture-card") as HTMLElement,
+        {
+            draggable: ".el-upload-list__item",
+            animation: 500,
+            onEnd({ newIndex, oldIndex }) {
+                const newList = [...fileList.value];
+                const currRow = newList.splice(oldIndex as number, 1)[0];
+                newList.splice(newIndex as number, 0, currRow);
+                fileList.value = newList;
+            },
         }
-    });
+    );
 };
 
 const loading = ref(false);
 
-
-const dialogImageUrl = ref('')
-const dialogVisible = ref(false)
-const disabled = ref(false)
+const dialogImageUrl = ref("");
+const dialogVisible = ref(false);
+const disabled = ref(false);
 
 const handleRemove = (file: UploadFile) => {
-    console.log(file)
+    console.log(file);
     // 使用 filter 方法过滤掉 uid 等于 file.uid 的对象
-    const newFileList = fileList.value.filter(item => item.uid !== file.uid);
+    const newFileList = fileList.value.filter((item) => item.uid !== file.uid);
     // 将新的文件列表设置回 fileList.value
     fileList.value = newFileList;
 };
 
-
-
 const handlePictureCardPreview = (file: UploadFile) => {
-    dialogImageUrl.value = file.url!
-    dialogVisible.value = true
-}
+    dialogImageUrl.value = file.url!;
+    dialogVisible.value = true;
+};
 
 const handleDownload = (file: UploadFile) => {
-    console.log(file)
-}
+    console.log(file);
+};
 
-const fileList = ref<UploadUserFile[]>([])
+const fileList = ref<UploadUserFile[]>([]);
 
 async function generateFiles(n) {
     loading.value = true;
     const files = [];
     //'https://api.yimian.xyz/img?type=wallpaper'
-    const url = ['https://api.yimian.xyz/img', 'https://www.dmoe.cc/random.php', 'https://api.r10086.com/%E6%A8%B1%E9%81%93%E9%9A%8F%E6%9C%BA%E5%9B%BE%E7%89%87api%E6%8E%A5%E5%8F%A3.php?%E5%9B%BE%E7%89%87%E7%B3%BB%E5%88%97=%E7%8E%8B%E8%80%85%E8%8D%A3%E8%80%80', 'https://api.r10086.com/%E6%A8%B1%E9%81%93%E9%9A%8F%E6%9C%BA%E5%9B%BE%E7%89%87api%E6%8E%A5%E5%8F%A3.php?%E5%9B%BE%E7%89%87%E7%B3%BB%E5%88%97=%E6%88%91%E7%9A%84%E4%B8%96%E7%95%8C%E7%B3%BB%E5%88%971'];
+    const url = [
+        "https://www.dmoe.cc/random.php",
+        "https://api.yimian.xyz/img",
+        "https://api.r10086.com/%E6%A8%B1%E9%81%93%E9%9A%8F%E6%9C%BA%E5%9B%BE%E7%89%87api%E6%8E%A5%E5%8F%A3.php?%E5%9B%BE%E7%89%87%E7%B3%BB%E5%88%97=%E7%8E%8B%E8%80%85%E8%8D%A3%E8%80%80",
+        "https://api.r10086.com/%E6%A8%B1%E9%81%93%E9%9A%8F%E6%9C%BA%E5%9B%BE%E7%89%87api%E6%8E%A5%E5%8F%A3.php?%E5%9B%BE%E7%89%87%E7%B3%BB%E5%88%97=%E6%88%91%E7%9A%84%E4%B8%96%E7%95%8C%E7%B3%BB%E5%88%971",
+    ];
 
     for (let i = 0; i < n; i++) {
-        const timestamp = new Date().toISOString().replace(/[-:.]/g, '').slice(0, 17); // 生成时间戳
+        const timestamp = new Date()
+            .toISOString()
+            .replace(/[-:.]/g, "")
+            .slice(0, 17); // 生成时间戳
         const random_number = Math.floor(1000 + Math.random() * 9000); // 添加随机数以确保唯一性
         const name = `${timestamp}_${random_number}.png`;
         let index = Math.floor(Math.random() * url.length);
 
         files.push({
             name: name as string,
-            url: `${url[index]}?${random_number}` as string,
+            url: index === 0 ? `${url[index]}?${random_number}` : `${url[index]}` as string,
         });
     }
     // 将生成的文件列表赋值给 fileList
     fileList.value.push(...files);
-    setTimeout(() => loading.value = false, 2000)
+    setTimeout(() => (loading.value = false), 2000);
 }
 
 const clearImg = () => {
     fileList.value = [];
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -132,7 +141,8 @@ const clearImg = () => {
     align-items: center;
     height: 100%;
 }
-:deep(.el-dialog__body){
+
+:deep(.el-dialog__body) {
     text-align: center;
 }
 </style>
